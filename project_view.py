ebf8de7d4573a24f199301eee9fc87e6ef590ee9
Beka Tomashvili <bekatomashvili@gmail.com>
2014-04-27 03:41:05 +0400

    added task list

--- project_view.py
+++ project_view.py
@@ -3,9 +3,8 @@ from flask import Blueprint, request, redirect, render_template, url_for, sessio
 from flask.views import MethodView
 from flask.ext.mongoengine.wtf import model_form
 from flaskstarter import app
-from models import User, Project
+from models import User, Project,Task
 import flask_sijax
-
 
 
 @app.route('/project/<project_id>', methods=['POST', 'GET'])
@@ -15,14 +14,22 @@ def project(project_id):
 
 
 @app.route('/tasks/<project_id>', methods=['POST', 'GET'])
-def tasks(project_id):
+def tasks_list(project_id):
     pro = Project.objects.get(pk=project_id)
-    return render_template('tasks/list.html',project=pro)
+    tasks = Task.objects(project=pro)
+    return render_template('tasks/list.html',project=pro,tasks=tasks)
 
 
 @app.route('/newtask/<project_id>', methods=['POST', 'GET'])
 def new_task(project_id):
     pro = Project.objects.get(pk=project_id)
+    if request.method == "POST":
+        task = Task()
+        task.title = request.form['title']
+        task.project = pro
+        task.description = request.form['description']
+        task.save()
+        return redirect(url_for('tasks_list',project_id=project_id))
     return render_template('tasks/new.html',project=pro)
 
 
